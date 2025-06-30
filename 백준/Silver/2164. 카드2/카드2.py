ad7ae48637d81af4e04a0b5f9e3160a0acf1e8ba
Keonwoo Roh <ro_keonwoo@korea.ac.kr>
2025-06-30 13:01:21 +0900

    [Silver IV] Title: 카드2, Time: 184 ms, Memory: 55920 KB -BaekjoonHub

---
+++ 백준/Silver/2164. 카드2/카드2.py
@@ -0,0 +1,14 @@
+import sys
+from collections import deque
+
+N = int(sys.stdin.readline().strip())
+
+
+queue = deque([i for i in range(1, N + 1)])
+
+while len(queue) > 1:
+    queue.popleft()
+    queue.append(queue.popleft())
+
+print(queue[0])
+
